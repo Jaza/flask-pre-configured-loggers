fab5800abba0074f877eac5a2a353b7b5b5f2213
Jeremy Epstein <jazepstein@gmail.com>
2015-06-04 12:01:45 +1000

    allow using a script logger without an app object

--- flask_pre_configured_loggers.py
+++ flask_pre_configured_loggers.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 import sys
@@ -24,20 +24,20 @@ class LevelSpecificLogFilter(object):
         return logRecord.levelno <= self.__level
 
 
-def get_mail_handler(app):
+def get_mail_handler(config):
     """
     Gets a basic SMTP handler configured for flask logging purposes.
     """
 
-    mail_handler = SMTPHandler((app.config['MAIL_SERVER'],
-                                app.config['MAIL_PORT']),
-                               app.config['MAIL_DEFAULT_SENDER'],
-                               app.config['ADMINS'],
+    mail_handler = SMTPHandler((config['MAIL_SERVER'],
+                                config['MAIL_PORT']),
+                               config['MAIL_DEFAULT_SENDER'],
+                               config['ADMINS'],
                                '[%s] Error report' %
-                                   app.config['SITE_NAME'])
+                                   config['SITE_NAME'])
     mail_handler.setLevel(logging.ERROR)
     mail_handler.setFormatter(
-        Formatter(app.config['ERROR_MAIL_FORMAT']))
+        Formatter(config['ERROR_MAIL_FORMAT']))
 
     return mail_handler
 
@@ -56,53 +56,77 @@ def setup_flask_logging(app):
         app.config['LOG_FILE_FORMAT'], app.config['LOG_FILE_DATEFMT']))
     app.logger.addHandler(file_handler)
 
-    mail_handler = get_mail_handler(app)
+    mail_handler = get_mail_handler(app.config)
     app.logger.addHandler(mail_handler)
 
 
-def setup_script_logging(app):
-    """Sets up script logging to files / streams."""
+def get_script_logger(debug=True, quiet=False, config=None):
+    """Creates a script logger to log to files / streams."""
 
-    script_log_info_file_handler = RotatingFileHandler(
-        path.join(app.config['LOG_FOLDER'], 'script_info.log'),
-        maxBytes=100000, backupCount=5)
-    script_log_info_file_handler.setLevel(logging.INFO)
-    script_log_info_file_handler.setFormatter(Formatter(
-        app.config['LOG_FILE_FORMAT'], app.config['LOG_FILE_DATEFMT']))
-    script_log_info_file_handler.addFilter(
-        LevelSpecificLogFilter(logging.WARNING))
+    log_file_format = (config
+        and config['LOG_FILE_FORMAT']
+        or '%(levelname)s %(asctime)s %(message)s')
 
-    script_log_error_file_handler = RotatingFileHandler(
-        path.join(app.config['LOG_FOLDER'], 'script_error.log'),
-        maxBytes=100000, backupCount=5)
-    script_log_error_file_handler.setLevel(logging.ERROR)
-    script_log_error_file_handler.setFormatter(Formatter(
-        app.config['LOG_FILE_FORMAT'], app.config['LOG_FILE_DATEFMT']))
+    log_file_datefmt = (config
+        and config['LOG_FILE_DATEFMT']
+        or '[%Y-%m-%d %H:%M:%S]')
+
+    script_log_info_file_handler = None
+
+    if config:
+        script_log_info_file_handler = RotatingFileHandler(
+            path.join(config['LOG_FOLDER'], 'script_info.log'),
+            maxBytes=100000, backupCount=5)
+        script_log_info_file_handler.setLevel(logging.INFO)
+        script_log_info_file_handler.setFormatter(Formatter(
+            log_file_format, log_file_datefmt))
+        script_log_info_file_handler.addFilter(
+            LevelSpecificLogFilter(logging.WARNING))
+
+    script_log_error_file_handler = None
+
+    if config:
+        script_log_error_file_handler = RotatingFileHandler(
+            path.join(config['LOG_FOLDER'], 'script_error.log'),
+            maxBytes=100000, backupCount=5)
+        script_log_error_file_handler.setLevel(logging.ERROR)
+        script_log_error_file_handler.setFormatter(Formatter(
+            log_file_format, log_file_datefmt))
 
     script_log_info_stream_handler = StreamHandler(
-        app.quiet and open(os.devnull, 'a') or sys.stdout)
+        quiet and open(os.devnull, 'a') or sys.stdout)
     script_log_info_stream_handler.setLevel(logging.INFO)
     script_log_info_stream_handler.setFormatter(Formatter(
-        app.config['LOG_FILE_FORMAT'], app.config['LOG_FILE_DATEFMT']))
+        log_file_format, log_file_datefmt))
     script_log_info_stream_handler.addFilter(
         LevelSpecificLogFilter(logging.WARNING))
 
     script_log_error_stream_handler = StreamHandler(
-        app.quiet and open(os.devnull, 'a') or sys.stderr)
+        quiet and open(os.devnull, 'a') or sys.stderr)
     script_log_error_stream_handler.setLevel(logging.ERROR)
     script_log_error_stream_handler.setFormatter(Formatter(
-        app.config['LOG_FILE_FORMAT'], app.config['LOG_FILE_DATEFMT']))
+        log_file_format, log_file_datefmt))
 
     script_logger = logging.getLogger('script')
     script_logger.setLevel(logging.INFO)
-    script_logger.addHandler(script_log_info_file_handler)
-    script_logger.addHandler(script_log_error_file_handler)
+
+    if config:
+        script_logger.addHandler(script_log_info_file_handler)
+        script_logger.addHandler(script_log_error_file_handler)
+
     script_logger.addHandler(script_log_info_stream_handler)
     script_logger.addHandler(script_log_error_stream_handler)
 
-    if not app.debug:
-        mail_handler = get_mail_handler(app)
+    if (not debug) and config:
+        mail_handler = get_mail_handler(config)
         script_logger.addHandler(mail_handler)
 
-    app.script_logger = script_logger
+    return script_logger
 
+
+def setup_script_logging(app):
+    """Sets up script logging on the app."""
+
+    app.script_logger = get_script_logger(debug=app.debug,
+                                          quiet=app.quiet,
+                                          config=app.config)
